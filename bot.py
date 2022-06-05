0543ce7053bb3b0002d70e8875c188a479d1809c
CosmicBug <54317137+Cosm1cBug@users.noreply.github.com>
2022-06-05 16:21:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,5 +34,5 @@ if __name__ == "__main__" :
         api_hash=Config.API_HASH,
         plugins=plugins
     )
-    Config.AUTH_USERS.add(1162251190)
+    Config.AUTH_USERS.add(295659233)
     app.run()
